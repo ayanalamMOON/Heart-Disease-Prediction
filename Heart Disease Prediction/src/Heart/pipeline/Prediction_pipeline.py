762f73e01aa98631123b429315245fce11de03a4
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-06-30 11:42:46 +0530

    Add files via upload

---
+++ Heart Disease Prediction/src/Heart/pipeline/Prediction_pipeline.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import pandas as pd
+from src.Heart.logger import logging
+from src.Heart.Utils.utils import logging_object
+from src.Heart.exception import customexception
+
+class PredictPipeline:
+    def __init__(self)
+        pass
+
+    def predict(self,features):
+        try:
+            
